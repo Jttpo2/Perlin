1c4670aa6114d11370004280812260ce3615b9ca
John Petersson <johnpetersson@gmail.com>
2017-03-04 00:55:10 +0100

    Noise + sine

--- perlin.js
+++ perlin.js
@@ -1,5 +1,6 @@
 var start = 0;
 var increment = 0.01;
+var noiseScale = 0.01;
 
 var xOff1 = 0;
 var xOff2 = 10000;
@@ -17,10 +18,13 @@ function draw() {
 	noFill();
 	beginShape();
 	let xOff1 = start;
+	let xOff2 = start;
 	for (let x=0; x<width; x++) {
-		let y = noise(xOff1) * height;
-		vertex(x, y);
-		xOff1 += increment;
+		let y = map(noise(xOff1), 0, 1, 0, height);
+		// vertex(x, y);
+		let ySine = map(sin(xOff1), -1, 1, -100, 100);
+		vertex(x, y + ySine);
+		xOff1 += noiseScale;
 	}
 	endShape();
 
