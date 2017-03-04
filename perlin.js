70cd3466c983fba22d4a388137e8391b136bd1b7
John Petersson <johnpetersson@gmail.com>
2017-03-04 01:31:19 +0100

    2D noise texture

--- perlin.js
+++ perlin.js
@@ -1,33 +1,35 @@
-var start = 0;
 var increment = 0.01;
-var noiseScale = 0.01;
-
-var xOff1 = 0;
-var xOff2 = 10000;
 
 function setup() {
 	createCanvas(
 		// window.innerWidth,
 		// window.innerHeight
-		200, 200
+		400, 400
 		);
 
 	pixelDensity(1);
 }
 
 function draw() {
+	let yOff = 0;
+	
 	loadPixels();
 
-	for (let x=0; x<width; x++) {
-		for (let y=0;y<height; y++) {
+	noiseDetail(6, 0.5);
+	for (let y=0;y<height; y++) {
+		let xOff = 0;
+		for (let x=0; x<width; x++) {
 			let index = (x + y * width) * 4;
-			let r = random(255);
+			let r = noise(xOff, yOff) * 255;
 
 			pixels[index+0] = r; // r
 			pixels[index+1] = r; // g
 			pixels[index+2] = r; // b
 			pixels[index+3] = 255;
+
+			xOff += increment;
 		}
+		yOff += increment;
 	}
 	updatePixels();
 }
